824b242b7ae362481a325c94d9a54c6e72aa7e95
abouh <abouhir14@gmail.com>
2021-03-13 01:36:21 +0100

    add ControllerAjouterClient.java and AjouterClientDocument.fxml

--- src/application/controller/ControllerOperationClient.java
+++ src/application/controller/ControllerOperationClient.java
@@ -1,11 +1,17 @@
 package application.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -19,11 +25,24 @@ public class ControllerOperationClient implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+    public void ajouterOnAction(ActionEvent event) throws IOException {
+        switchStage("/resource/fxml/AjouterClientDocument.fxml");
+    }
+
     public void btncloseOnMouseEvent(MouseEvent event){
         close();
     }
     public void btnreduceOnMouseEvent(MouseEvent event){reduce();}
 
+
+    public void switchStage(String name) throws IOException {
+        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource(name));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setScene(new Scene(root, 948, 687));
+        primaryStage.show();
+    }
     public void close(){
         Stage stage =(Stage)close.getScene().getWindow();
         stage.close();
