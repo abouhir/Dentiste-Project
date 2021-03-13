3b4f3b0aa12bfa6724fe8925c5bfe443cb4ffdf2
abouh <abouhir14@gmail.com>
2021-03-13 01:38:04 +0100

    add Action to btn ajouter

--- src/application/controller/ControllerOperationClient.java
+++ src/application/controller/ControllerOperationClient.java
@@ -1,5 +1,6 @@
 package application.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -10,7 +11,6 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
