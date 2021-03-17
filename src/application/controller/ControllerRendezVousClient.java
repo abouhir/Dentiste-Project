9a5c46975cadddde0c8dff4f2dae87befae810a2
abouh <abouhir14@gmail.com>
2021-03-17 15:31:57 +0100

    add rendez vous action

--- src/application/controller/ControllerRendezVousClient.java
+++ src/application/controller/ControllerRendezVousClient.java
@@ -1,21 +1,28 @@
 package application.controller;
 
+import application.dal.model.RendezVous;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
 import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ControllerRendezVousClient implements Initializable {
+    private RendezVous rdv;
 
     @FXML
     private JFXButton btnAnnuler;
-
+    @FXML
+    private DatePicker txtDateRdv;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    public void rendezvousOnAction(){
 
     }
 
